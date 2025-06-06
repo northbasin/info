0c5da519208c5651aa9a7b961176f30ab1ecba6f
John Kenny <johnkenny54@yahoo.com>
2025-06-06 13:34:36 -0700

    fix: fix path for GitHub pages

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,11 @@
+import { HtmlBasePlugin } from "@11ty/eleventy";
+
+export const config = {
+  pathPrefix: "/info/",
+};
+
 export default async function (eleventyConfig) {
+  eleventyConfig.addPlugin(HtmlBasePlugin);
   eleventyConfig.setInputDirectory("./content/**");
   eleventyConfig.setLayoutsDirectory("./layouts");
   eleventyConfig.addPassthroughCopy("./content/css");
